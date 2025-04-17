45cb15ec942ddef7dac0e3e6ba33b5798ce4b34c
laich <laichloe@ufl.edu>
2025-04-17 19:58:55 -0400

    Created function that returns list of magnitude based on given location

--- Website/src/datasort.py
+++ Website/src/datasort.py
@@ -5,8 +5,19 @@ import sklearn as sk
 
 #Citations: https://pandas.pydata.org/docs/user_guide/groupby.html
 
+# sort by location
+def findLocation(table, input):
+    final = []
+    for i, row in table.iterrows():
+        if row["location.name"] == input:
+            final.append(row["impact.magnitude"])
+    return final
+
 data = pd.read_csv("data/earthquakes.csv")
 
+#new data table for sorted methods
 selected = ["id", "impact.magnitude", "location.latitude", "location.longitude", "location.name"]
 new_data = data[selected]
-print(new_data)
+
+mag = findLocation(new_data, "Alaska")
+print(mag)
