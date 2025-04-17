7ffeef854eaeac65ff0b1e8f468a2472baf21e87
laich <laichloe@ufl.edu>
2025-04-17 17:01:19 -0400

    Organized the data to create new dataframe which we can use to better organize

--- Website/src/datasort.py
+++ Website/src/datasort.py
@@ -3,4 +3,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 import sklearn as sk
 
+#Citations: https://pandas.pydata.org/docs/user_guide/groupby.html
+
 data = pd.read_csv("data/earthquakes.csv")
+
+selected = ["id", "impact.magnitude", "location.latitude", "location.longitude", "location.name"]
+new_data = data[selected]
+print(new_data)
